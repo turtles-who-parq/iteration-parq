21ac7e12388a711ce7b355951263dd1c7ea762ee
Khang Sabre-Nguyen <klsabren.7@outlook.com>
2022-07-19 21:16:21 -0400

    prior to updating dev

--- Application/server/controllers/stripeController.js
+++ Application/server/controllers/stripeController.js
@@ -1,2 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 const stripe = require('stripe');
 const stripeSecret = process.env.STRIPE_API_KEY;
+
+const express = require('express');
+const app = express();
